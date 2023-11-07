dec5d2d960cabf431ecfeadaca72a0cc3f088388
sep.vaz <g12sep@gmail.com>
2023-11-08 00:15:22 +0100

    lite css

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const slide = document.querySelector(".container img")
-const imgArray = ["images/GUSC.jpg", "images/GUSC1.jpg", "images/GUSC2.jpg", "images/GUSC3.jpg", "images/GUSC4.jpg"]
+const imgArray = ["images/GUSC1.jpg", "images/GUSC2.jpg", "images/GUSC3.jpg", "images/GUSC4.jpg"]
 const buttonOne = document.querySelector(".btn-one")
 const buttonTwo = document.querySelector(".btn-two")
 
 let index = 0;
-
-
-buttonOne.addEventListener("click", function)
 
 buttonTwo.addEventListener("click", function(){
     index++;
@@ -17,5 +14,17 @@ buttonTwo.addEventListener("click", function(){
     slide.src = imgArray[index];
 
 })
+
+buttonOne.addEventListener("click", function(){
+    index--;
+
+    if (index < 0) {
+        index = imgArray.length - 1
+    }
+    
+    slide.src = imgArray[index];
+
+})
+
 
 
